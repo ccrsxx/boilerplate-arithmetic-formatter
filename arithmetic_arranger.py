ce69de48797b60afa2df6cd34d12b885b16d3fab
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-07-22 14:29:57 +0700

    test

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,16 @@
 def arithmetic_arranger(problems):
 
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
+    
+    for num in problems:
+        if '+' not in num and '-' not in num:
+            return "Error: Operator must be '+' or '-'."
+
 
     return arranged_problems
+
+# probs = ['1 + 2', '2 + 3', '3 + 4', '4 + 5', '5 + 6', '6 + 5']
+probs = ['1 + 2', '2 + 3', '3 + 4', '4 + 5', '5 + 6']
+check = arithmetic_arranger(probs)
+print(check)
