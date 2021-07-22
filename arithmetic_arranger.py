06b18b1041945830d712fed7ce06dc31bbf92eb9
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-07-22 15:51:09 +0700

    will do more tonight

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -3,12 +3,23 @@ def arithmetic_arranger(problems):
     if len(problems) > 5:
         return 'Error: Too many problems.'
     
-    for num in problems:
-        if '+' not in num and '-' not in num:
+    for i in range(len(problems)):
+        prob = problems[i].split()
+        
+        fn = prob[0]
+        op = prob[1]
+        sn = prob[2]
+
+        if '+' not in op and '-' not in op:
             return "Error: Operator must be '+' or '-'."
 
+        elif len(fn) > 4 or len(sn) > 4:
+            return "Error: Numbers cannot be more than four digits."
 
-    return arranged_problems
+        elif not fn.isdigit() or not sn.isdigit():
+            return "Error: Numbers must only contain digits."
+        
+
 
 # probs = ['1 + 2', '2 + 3', '3 + 4', '4 + 5', '5 + 6', '6 + 5']
 probs = ['1 + 2', '2 + 3', '3 + 4', '4 + 5', '5 + 6']
